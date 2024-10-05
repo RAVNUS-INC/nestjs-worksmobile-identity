89861cb697941b1aa5fd94329117fbc9f657addf
getsolaris <getsolaris.kr@gmail.com>
2024-10-06 00:02:51 +0900

    feat: sendTextMessage 구현

--- src/shared/worksmobile/worksmobile-bot.service.spec.ts
+++ src/shared/worksmobile/worksmobile-bot.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WorksmobileBotService } from './worksmobile-bot.service';
+import { ConfigModule } from '@nestjs/config';
+import { WorksmobileTokenService } from './worksmobile-token.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 describe('WorksmobileBotService', () => {
   let service: WorksmobileBotService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WorksmobileBotService],
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+        CacheModule.register(),
+      ],
+      providers: [WorksmobileBotService, WorksmobileTokenService],
     }).compile();
 
     service = module.get<WorksmobileBotService>(WorksmobileBotService);
@@ -14,5 +23,20 @@ describe('WorksmobileBotService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('sendTextMessage()', () => {
+    it('텍스트형 메시지 발송 성공', async () => {
+      jest.spyOn(service, 'sendTextMessage').mockResolvedValue(true);
+      const result = await service.sendTextMessage('test message');
+      expect(result).toBe(true);
+    });
+
+    it('텍스트형 메시지 발송 실패', async () => {
+      jest
+        .spyOn(service, 'sendTextMessage')
+        .mockRejectedValue(new Error('메시지 전송에 실패했습니다.'));
+      await expect(service.sendTextMessage('')).rejects.toThrow();
+    });
   });
 });
--- src/shared/worksmobile/worksmobile-bot.service.ts
+++ src/shared/worksmobile/worksmobile-bot.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { generateBotsUsersMessages } from './constants/endpoint.constant';
+import { ConfigService } from '@nestjs/config';
+import { WorksmobileTokenService } from './worksmobile-token.service';
 
 @Injectable()
-export class WorksmobileBotService {}
+export class WorksmobileBotService {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly worksmobileTokenService: WorksmobileTokenService,
+  ) {}
+
+  async sendTextMessage(text: string): Promise<boolean> {
+    const botId = this.config.get('NAVER_WORKS_BOT_ID');
+    const testUserId = this.config.get('RAVNUS_TEST_USER_ID');
+    const token = await this.worksmobileTokenService.getToken();
+
+    try {
+      await axios.post(
+        generateBotsUsersMessages(botId, testUserId),
+        {
+          content: {
+            type: 'text',
+            text,
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      return true;
+    } catch (e) {
+      throw new Error(
+        `메시지 전송에 실패했습니다. (${e.response.data?.message || e.response.data?.description})`,
+      );
+    }
+  }
+}
